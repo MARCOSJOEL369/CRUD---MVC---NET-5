f4e7c41628cb05e9e52d036e4475b37235001712
marco <marco@DESKTOP-RAFDGVR>
2022-07-16 15:05:54 -0400

    Add project files.

---
+++ CRUD --MVC --NET 5/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CRUD___MVC___NET_5.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ CRUD --MVC --NET 5/Models/Vehicle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUD___MVC___NET_5.Models
+{
+    public class Vehicle
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Llenar el campo es obligatorio")]
+        [StringLength(50, ErrorMessage = "Obligatorio cumplir de 5 a 50 caracteres", MinimumLength = 5)]
+        [Display(Name = "Marca")]
+        public int marca { get; set; }
+
+        [Required(ErrorMessage = "Llenar el campo es obligatorio")]
+        [StringLength(50, ErrorMessage = "Obligatorio cumplir de 5 a 50 caracteres", MinimumLength = 5)]
+        [Display(Name = "color")]
+        public string color { get; set; }
+
+        [Required(ErrorMessage = "Llenar el campo es obligatorio")]
+        [Display(Name = "kilometraje")]
+        public int kilometraje { get; set; }
+
+        [Required(ErrorMessage = "Llenar el campo es obligatorio")]
+        [Display(Name = "AñoDeLanzamineto")]
+        [DataType(DataType.Date)]
+        public DateTime AñoDeLanzamineto { get; set; }
+    }
+}
